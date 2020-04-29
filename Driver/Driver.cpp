8237d1ffdf8b66d254bb23198321ead422575cc1
numver8638 <numver8638@naver.com>
2020-04-29 17:18:09 +0900

    Add subprojects

---
+++ Driver/Driver.cpp
@@ -0,0 +1,13 @@
+/*
+ * Driver.cpp
+ * - Main executable file.
+ *
+ * Copyright (c) 2019~2020 numver8638(신진환, Jinhwan Shin)
+ * Released under the MIT License.
+ * See the LICENSE file in the project root to get more information.
+ */
+#include <iostream>
+
+int main(int argc, const char* argv[], const char* envp[]) {
+    return 0;
+}
---
+++ Test/Driver.cpp
@@ -0,0 +1,10 @@
+/*
+ * Driver.cpp
+ * - Main executable for test.
+ *
+ * Copyright (c) 2019~2020 numver8638(신진환, Jinhwan Shin)
+ * Released under the MIT License.
+ * See the LICENSE file in the project root to get more information.
+ */
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
