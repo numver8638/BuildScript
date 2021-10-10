e2075ea5b340058a00d3d686006ce298f1d61f4c
numver8638 <numver8638@naver.com>
2021-10-10 22:41:17 +0900

    Update Assert.h
    Change 'NEVER_BE_NULL' macro to accept message.

--- Header/BuildScript/Assert.h
+++ Header/BuildScript/Assert.h
@@ -11,8 +11,8 @@
 
 #include <cassert>
 
-#define NOT_REACHABLE       (assert(!"never reached."))
-#define NEVER_BE_NULL(e)    (assert((e) != nullptr && "never be null."))
-#define NEVER_BE_CALLED     (assert(!"never be called. check the code."))
+#define NOT_REACHABLE           (assert(!"never reached."))
+#define NEVER_BE_NULL(e)        (assert((e) != nullptr && "never be null."))
+#define NEVER_BE_CALLED(msg)    (assert(!"never be called - " msg " check the code."))
 
 #endif // BUILDSCRIPT_ASSERT_H
