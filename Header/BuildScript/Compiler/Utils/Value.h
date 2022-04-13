e04ab1ba051f7d1108631c80f5f1248e27a99293
numver8638 <numver8638@naver.com>
2022-04-13 23:38:01 +0900

    [Compiler] Update Value.h
    - Changed Value to optional of variant to represent uninitalized state.
    - Added std::monostate to variant to represent empty value(none in BuildScript).

--- Header/BuildScript/Compiler/Utils/Value.h
+++ Header/BuildScript/Compiler/Utils/Value.h
@@ -10,6 +10,7 @@
 #define BUILDSCRIPT_COMPILER_UTILS_VALUE_H
 
 #include <cstdint>
+#include <optional>
 #include <string>
 #include <string_view>
 #include <variant>
@@ -18,25 +19,9 @@ namespace BuildScript {
     /**
      * @brief Represents constant value evaluated during compile.
      */
-    struct Value : public std::variant<bool, int64_t, double, std::string> {
-        using super = std::variant<bool, int64_t, double, std::string>;
+    using Value = std::optional<std::variant<std::monostate, bool, int64_t, double, std::string>>;
 
-        using super::variant;
-
-
-        constexpr Value(int i)
-            : variant(int64_t(i)) {}
-
-        Value(std::string_view str)
-            : variant(std::string(str)) {}
-    }; // end struct Value
+    constexpr auto None = std::monostate();
 } // end namespace BuildScript
-
-template <>
-struct std::hash<BuildScript::Value> {
-    size_t operator ()(const BuildScript::Value& val) const noexcept {
-        return std::hash<BuildScript::Value::super>()(val);
-    }
-};
 
 #endif // BUILDSCRIPT_COMPILER_UTILS_VALUE_H
